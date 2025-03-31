d1cbc5a41d4fbf819bd0f3fcb5fbf91dd4694ea9
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-03-31 10:34:00 -0300

    Add validators

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -5,8 +5,23 @@ const livroSchema = new mongoose.Schema(
     id: {type: String},
     titulo: {type: String, required: [true, "O título do livro é obrigatório!"]},
     autor: {type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: [true, "O(a) autor(a) é obrigatório!"]},
-    editora: {type: String, required: [true, "A editora é obrigatória!"]},
-    numeroPaginas: {type: Number}
+    editora: {
+      type: String,
+      required: [true, "A editora é obrigatória!"],
+      enum: { 
+        values: ["Casa do código", "Alura", "Classicos"],
+        message: "A editora {VALUE} não é um valor permitido"
+      }
+    },
+    numeroPaginas: {
+      type: Number,
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+      },
+      message: "O número de páginas deve estar entre 10 e 5000"
+    }
+    }
   }
 );
 
